789076731bb4f45244468acf1992712982a321f5
Jack Sleight <jacksleight@gmail.com>
2022-02-21 10:16:57 +0000

    Use array_merge

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -22,7 +22,7 @@ class ServiceProvider extends LaravelServiceProvider
             list($name, $data) = explode(',', $expression, 2) + [null, '[]'];
             $name = trim($name, '\'" ');
 
-            return '<?php $__snip_'.$name.'(\Illuminate\Support\Arr::except(get_defined_vars(), ["__data"]) + '.$data.') ?>';
+            return '<?php $__snip_'.$name.'(array_merge(\Illuminate\Support\Arr::except(get_defined_vars(), ["__data"]), '.$data.')) ?>';
         });
     }
 }
